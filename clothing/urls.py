2c239470c8a97f0162e42964ab413fef7c221b33
Amogh Shukla <amoghshukla548@gmail.com>
2025-02-24 14:28:13 +0530

    Final Commit

--- clothing/urls.py
+++ clothing/urls.py
@@ -3,4 +3,6 @@ from clothing import views
 
 urlpatterns = [
     path('', views.clothing, name='clothing'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- clothing/views.py
+++ clothing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from clothing.models import product
 from .forms import ProductForm
 
@@ -13,5 +13,27 @@ def clothing(request):
             form.save()
             form = ProductForm()
     return render(request, "clothing/fashion.html", {"pro_img":pro_img, "form":form})
+
+def edit_product(request, product_id):
+    Product = get_object_or_404(product, id=product_id)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=Product) # request.POST = Text File, 
+        if form.is_valid():
+            form.save()
+            return redirect('clothing')
+    else:
+        form = ProductForm(instance=Product)
+    
+    return render(request, 'clothing/edit_product.html',{'form':form})
+
+def delete_product(request, product_id):
+    Product = get_object_or_404(product, id=product_id)
+
+    if request.method == "POST":
+        Product.delete() 
+        return redirect('clothing')
+    
+    return render(request, 'clothing/confirm_delete.html',{'product':product})
 
     
