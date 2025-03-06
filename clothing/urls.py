116a234dc775c41e5ea2abdeca1421ddd6903cdc
Amogh Shukla <110371842+AmoghShukla@users.noreply.github.com>
2025-03-06 23:01:22 +0530

    Update urls.py

--- clothing/urls.py
+++ clothing/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.clothing, name='clothing'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('', views.fashion, name='home'),  # Homepage (Fashion)
-    path('about/', views.about, name='about'),  # About Page
-    path('contact/', views.contact, name='contact'),  # Contact Page
-    path('home/', views.home_flipkart, name='home_flipkart'),  # Home Page
+    path('', views.fashion, name='home'),  
+    path('about/', views.about, name='about'),  
+    path('contact/', views.contact, name='contact'),  
+    path('home/', views.home_flipkart, name='home_flipkart'),  
 ]
